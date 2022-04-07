f59083577571d4d125f264eb85907f1fd35d0c27
Mohammd Mustafa <momust187@gmail.com>
2022-04-07 20:51:08 +0200

    Fix: productDetails

--- group-project/strapi API/productDetails.js
+++ group-project/strapi API/productDetails.js
@@ -1,60 +1,88 @@
 
+     let productUrl = '';
+ let myId = 0;
+//   function getInfo(props){
+//     myId = props;
+//     laptopsUrl = '/'+ props
+//     console.log('laptopsUrl'+ laptopsUrl);
+//     console.log('props'+ props);
+//     renderObjects();
 
+// }
+
+function getInfo(props){
+    myId = props;
+   productUrl = '/'+ props
+    console.log(productUrl);
+    localStorage.setItem('objId', productUrl);
+    // renderObjects(laptopsUrl);
+    
+
+}
 
 async function renderObjects(){
-    let productUrl = localStorage.getItem('objId');
-    console.log(productUrl);
+    
+    
     let apiUrl = "http://localhost:1337";
     console.log(apiUrl);
-
-    let urlLocalhost = `http://localhost:1337/api/Laptops${productUrl}?populate=image`;
-
+    console.log('test'+productUrl);
+   
+    let urlLocalhost = apiUrl +`/api/Laptops${productUrl}?populate=image`;
+    console.log('Hello' + productUrl)
     let stringResponse = await fetch (urlLocalhost);
     let myobject = await stringResponse.json();
     let output = '';
     let index = 0;
-     let title  = '';
-    let details  = '';
-    let price  = 0;
-    let qty  = 0;
-    let image = '';
+    
     //Kolla om data är en array
     if(Array.isArray(myobject.data)){
         myobject.data.forEach(element => {
             
             let attr = element.attributes;
-            console.log(element);
-
-            output += `
-                <div class="grid-item">
-                    <div class="laptop-image">
-                        <img src="${images[index].image}"></img>
-                    </div>
-                    <div class="item-info">
-                        <div class="item-title">${attr.title}</div>
-                        <div>Price: ${attr.price}</div>
-                        <div>Qty: ${attr.qty}</div>
-                    </div>
-                    
-                </div>
+            if (attr.image.data === null){
+                 img = 'https://res.cloudinary.com/dfqx0ptfj/image/upload/v1649245136/white_omnxo9.jpg';
                 
+            }else{
+                img = attr.image.data.attributes.formats.medium.url;
+            }
+            console.log(element);
+            
+            output += `
+            
+            <div class="col-4" onclick="getInfo(${element.id})">
+            <div class="card h-100 shadow-sm"> <img src="${img}" class="card-image-top"/>
+              <div class="card-body">
+                <div class="clearfix mb-3"> <span class="float-start badge rounded-pill bg-primary">Qty:${attr.qty}</span> <span class="float-end price-hp">${attr.price}kr</span> </div>
+                    <h5 class="card-title">${attr.title}</h5>                  
+                    </div>                               
+                </div>
+            </div>
             `;
+             
                index++;
+              
         });
     }else{
+        
         let object = myobject.data.attributes;
-       
-         title += `<div class="title"> Title: ${object.title}</div>`;
-         details += `<div> Description: ${object.description}</div>`;
-         image += `<img src="${object.image.data.attributes.formats.small.url}">`;
-         price += `<div> Price: ${object.price}</div>`;
-         qty+= `<div> In stock: ${object.qty}</div>`;
+        
+         output += `<div class="grid-item">
+                    <div class="laptop-image">
+                        <img src="${laptopsImages[myId-1].image}"></img>
+                    </div>
+                    <div class="item-info">
+                        <div class="item-title">${object.title}</div>
+                        <div>Price: ${object.price}</div>
+                        <div>Qty: ${object.qty}</div>
+                    </div>
+                    
+                </div>`;
     }
-     document.getElementById("title").innerHTML = title;
-     document.getElementById("details").innerHTML = details;
-     document.getElementById("image").innerHTML = image;
+    document.getElementById('output').innerHTML = output;
      
 }
 
 renderObjects();
+
+
 
